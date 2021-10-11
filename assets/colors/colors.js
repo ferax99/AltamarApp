a33574c6c1bee89917b28f876260b5aea660183b
ssg-13 <sebas.mqt@gmail.com>
2021-10-11 11:41:14 -0600

    se agrega fuentes y colores

---
+++ assets/colors/colors.js
@@ -0,0 +1,10 @@
+const colors = {
+    blackText: '#0C1722',
+    grayText: '#838383',
+    background: '#FBFAF9',
+    orangeUI: '#EE7333',
+    blueUI: '#00A3FF',
+    whiteButtons: '#FFFFFF',
+};
+
+export default colors;
