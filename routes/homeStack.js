569ebc99f878c83a3af65a77e9df0722e0c2a223
ferax99 <ferbin99@gmail.com>
2021-11-09 15:01:16 -0600

    correccion errores

---
+++ routes/homeStack.js
@@ -0,0 +1,43 @@
+import { createStackNavigator } from "react-navigation-stack";
+import { createAppContainer, StackRouter } from "react-navigation";
+import Login from '../screens/login';
+import Info from "../screens/info";
+import LoginPw from "../screens/loginPw";
+import Home from "../screens/home";
+import ChooseRole from "../screens/chooseRole";
+import CreateAccountSeller from "../screens/createAccountSeller";
+import CreateAccountBuyer from "../screens/createAccountBuyer";
+import MyProducts from "../screens/misProductos"
+import AddProducts from "../screens/addProducts";
+import EditProducts from "../screens/editProducts";
+import { defineAnimation } from "react-native-reanimated";
+import Favoritos from "../screens/favoritos";
+import Perfil from "../screens/editUserP";
+import KK from "../screens/perfil"
+
+
+const HomeStack = createStackNavigator({
+  Home: Home,
+  "Mis productos": MyProducts,
+  Favoritos:Favoritos,
+  Editor:EditProducts,
+  //X:Perfil,
+  Login: Login,
+  Perfil: KK,
+  Info: Info,
+  ChooseRole: ChooseRole,
+  X:Home,
+  "Creando Vendedor": CreateAccountSeller,
+  "Creando Comprador": CreateAccountBuyer,
+  LoginPw: LoginPw,
+  "Añadir productos": AddProducts,
+ 
+},
+  {
+    headerMode: 'false',
+    defaultNavigationOptions: { headerTitleAlign: 'center' }
+
+  });
+
+
+export default createAppContainer(HomeStack);
