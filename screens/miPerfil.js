458598e3691c547fd4765bc400b9aec893cad05e
ssg-13 <sebas.mqt@gmail.com>
2021-11-11 13:57:43 -0600

    cerrar sesion

--- screens/miPerfil.js
+++ screens/miPerfil.js
@@ -1,18 +1,76 @@
 import React from "react";
-import { View , ScrollView} from "react-native";
+import { View, ScrollView, StyleSheet, Text } from "react-native";
 import MiInfo from "../components/miInfo";
 import Frecuentes from "../components/frecuentes"
+import { TouchableOpacity } from "react-native-gesture-handler";
+import colors from "../assets/colors/colors";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import UserData from "../userData"
 
 
+const MiPerfil = ({ navigation }) => {
 
-const MiPerfil=({ navigation })=>{
-    return(
+    const vaciar = async () => {
+        try {
+
+            // await AsyncStorage.setItem("rol","vendedor");
+
+
+            await AsyncStorage.removeItem("telefono");
+            await AsyncStorage.removeItem("id");
+            await AsyncStorage.removeItem("nombre");
+            await AsyncStorage.removeItem("rol");
+            await AsyncStorage.removeItem("ubicacion");
+            await AsyncStorage.removeItem("cedula");
+
+            await AsyncStorage.removeItem("color");
+
+        } catch (err) {
+            console.log(err)
+        }
+
+    }
+
+    return (
         <View>
-            <MiInfo navigation={navigation}/>
+            <MiInfo navigation={navigation} />
             <ScrollView>
                 <Frecuentes />
             </ScrollView>
+            <TouchableOpacity
+                style={UserData.rol._W == "comprador" ? styles.botonComprador : styles.botonVendedor}
+                onPress={() => console.log("me manoseo")} >
+                <Text style={{ fontWeight: "bold", color: "#FFFFFF" }}>
+                    Cerrar sesión
+                </Text>
+            </TouchableOpacity>
         </View>
+
     )
 }
+
+const styles = StyleSheet.create({
+    botonComprador: {
+        backgroundColor: colors.orangeUI,
+        height: 48,
+        width: "90%",
+        alignItems: 'center',
+        padding: 15,
+        color: "#FFFFFF",
+        marginLeft: "5%",
+        marginTop: "5%",
+        borderRadius: 50,
+    },
+    botonVendedor: {
+        backgroundColor: colors.blueUI,
+        height: 48,
+        width: "90%",
+        alignItems: 'center',
+        padding: 15,
+        color: "#FFFFFF",
+        marginLeft: "5%",
+        marginTop: "5%",
+        borderRadius: 50,
+    },
+});
 export default MiPerfil
